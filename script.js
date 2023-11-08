e8210bf778816af30ab957c42434f3f8e501be35
Rohan Kar <realrohankar03@gmail.com>
2023-11-08 22:41:04 +0530

    added video with animated play button

---
+++ script.js
@@ -0,0 +1,35 @@
+var videocon = document.querySelector("#vid-container")
+var playbtn = document.querySelector(".play")
+
+
+videocon.addEventListener("mouseenter", function(){
+   gsap.to(playbtn, {
+    scale:1,
+    opacity:0.8
+   })
+})
+
+videocon.addEventListener("mouseleave", function(){
+    gsap.to(playbtn, {
+        scale:0,
+        opacity:0
+    })
+})
+
+videocon.addEventListener("mousemove", function(dets){
+    // gsap.to(playbtn, {
+    //     left:dets.x -1,
+    //     top:dets.y -8,
+    //     duration: 0.2
+    //}) 
+
+    const circleRadius = playbtn.clientWidth /2 ;
+
+    gsap.to(playbtn, {
+        left: dets.clientX - circleRadius,
+        top: dets.clientY - circleRadius,
+        duration: 0.5
+    })
+    
+})
+
